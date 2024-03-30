ae7a5736879daa03ec65a6af266c779ca448d608
Joseph Riddle <joeriddles10@gmail.com>
2024-03-30 07:51:17 -0700

    Refactor css modules

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,41 +1,41 @@
 // Footer.js
 import React from 'react'
 import { FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa'
-import './Footer.css'
+import styles from './Footer.module.css'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-            <ul className='social-icon'>
-                <li className='social-icon__item'>
-                    <a className='social-icon__link' href="https://www.youtube.com/channel/UCRtyWekZNAPNUySmpt3i31w?sub_confirmation=1">
+        <footer className={styles.footer}>
+            <ul className={styles['social-icon']}>
+                <li className={styles['social-icon__item']}>
+                    <a className={styles['social-icon__link']} href="https://www.youtube.com/channel/UCRtyWekZNAPNUySmpt3i31w?sub_confirmation=1">
                         <FaYoutube/>
                     </a>
                 </li>
-                <li className='social-icon__item'>
-                    <a className='social-icon__link' href='https://www.instagram.com/late_night_woodworking'>
+                <li className={styles['social-icon__item']}>
+                    <a className={styles['social-icon__link']} href='https://www.instagram.com/late_night_woodworking'>
                     <FaInstagram/>
                     </a>
                 </li>
-                <li className='social-icon__item'>
-                    <a className="social-icon__link" href="https://www.tiktok.com/@late_night_woodworking">
+                <li className={styles['social-icon__item']}>
+                    <a className={styles['social-icon__link']} href="https://www.tiktok.com/@late_night_woodworking">
                     <FaTiktok/>
                     </a>
                 </li>
             </ul>    
-                <ul class="menu">
-                    <li class="menu__item">
-                        <Link class="menu__link" to="/">Home</Link>
+                <ul className={styles['menu']}>
+                    <li className={styles['menu__item']}>
+                        <Link className={styles['menu__link']} to="/">Home</Link>
                     </li>
-                    <li class="menu__item">
-                        <Link class="menu__link" to="/shop">Shop</Link>
+                    <li className={styles['menu__item']}>
+                        <Link className={styles['menu__link']} to="/shop">Shop</Link>
                     </li>
-                    <li class="menu__item">
-                        <Link class="menu__link" to="/about">About</Link>
+                    <li className={styles['menu__item']}>
+                        <Link className={styles['menu__link']} to="/about">About</Link>
                     </li>
-                    <li class="menu__item">
-                        <Link class="menu__link" to="/about">Contact</Link>
+                    <li className={styles['menu__item']}>
+                        <Link className={styles['menu__link']} to="/about">Contact</Link>
                     </li>
                 </ul>
             <p>&copy; 2024 Late Night Woodworking</p>
--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,41 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import './Header.scss'
+import styles from './Header.module.scss'
 
 const Header = () => {
     const location = useLocation()
+
+    const burgerRef = useRef(null);
 
 	const [click, setClick] = useState(false);
     const handleBurgerClick = () => {
 		setClick(!click);
 
 		// Burger Animation
-		const burger = document.querySelector('.burger');
-		burger.classList.toggle('toggleBurger');
+		burgerRef.current.classList.toggle('toggleBurger');
 	}
 
     const isHome = location.pathname === '/'
 
     return (
-        <div className='nav-wrapper'>
-            <nav className={isHome ? 'home-nav' : ''}>
-                <Link to="/" className='logo-container'>
-                    <img className='logo' src={isHome ? '/assets/logo.png' : '/assets/logo_black.png'} alt="Logo" />
+        <div className={styles['nav-wrapper']}>
+            <nav className={isHome ? styles['home-nav'] : ''}>
+                <Link to="/" className={styles['logo-container']}>
+                    <img className={styles['logo']} src={isHome ? '/assets/logo.png' : '/assets/logo_black.png'} alt="Logo" />
                 </Link>
-                <div onClick={handleBurgerClick} className="burger"> 
+                <div ref={burgerRef} onClick={handleBurgerClick} className={styles['burger']}> 
                     <div className="line1"></div>
                     <div className="line2"></div>
                     <div className="line3"></div>
                 </div>
-                <ul className={click ? 'nav active' : 'nav'}>
+                <ul className={click ? `${styles.nav} ${styles.active}` : styles.nav}>
                     <li onClick={handleBurgerClick}>
-                        <Link className='nav-item' to="/">Home</Link>
+                        <Link className={styles['nav-item']} to="/">Home</Link>
                     </li>
                     <li onClick={handleBurgerClick}>
-                        <Link className='nav-item' to="/shop">Shop</Link>
+                        <Link className={styles['nav-item']} to="/shop">Shop</Link>
                     </li>
                     <li onClick={handleBurgerClick}>
-                        <Link className='nav-item' to="/about">About</Link>
+                        <Link className={styles['nav-item']} to="/about">About</Link>
                     </li>
                 </ul>
             </nav>
