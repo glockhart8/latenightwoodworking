e0b2a63070193e99974b1860a706bf3000c0db5e
Griffin Lockhart <lockhartgriffin@yahoo.com>
2024-06-25 18:48:42 -0700

    dice_tower about_me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,33 +1,71 @@
 import React from 'react'
 
+import '../components/About/About.css'
+
 const AboutPage = () => {
     return (
-        <div>
-            <h1>About Me and My "Woodworking Studio"</h1>
-            <p>
-                Where creativity meets the quiet hours. I have always found that I have this 
-                burst of creativity super late at night, with whatever I am doing. <br></br>
-                <br></br>
-                It May be editing a video, doing the actual woodworking, photography, drawing, 
-                or even coding... as I'm currently doing and it's midnight. 
-                <br></br>
-                Late Night Woodworking came along in 2023, and it's here to stay. 
-                <br></br>
-                <br></br>
-                <br></br>
-                I'll add some more stuff on here when I get the time, I'm writing the code myself. 
-                For now I just need it to get the job done, I'm sorry for all the bugs it still has.
-                <br></br>
-                <br></br>
-                For now, this will have to do. If you have any questions or inquiries, 
-                email me at <b>contact@latenightwoodworking.com</b>
-                <br></br>
-                or go to my instagram and DM on there.
-                <br></br>
-                <br></br>
-                :)
-            </p>
-            
+        <div className='about-wrapper'>
+            <div className='text'>
+                <h1>My Story.</h1>
+                <p> Where do I begin… It was a chilly Seattle evening in 1997… no, that’s too far back. But not by much.
+                    <br></br><br></br>
+                    While Late Night Woodworking was established in December of 2023, the long winding story of where I 
+                    am today begins long before that.
+                    <br></br><br></br>
+                    Growing up, I always enjoyed making things. The first thing I remember making was a catapult, I was 
+                    in the 5th Grade. I remember being really proud of it, I think that feeling sticks with me today with 
+                    everything I make, and it’s what has kept me making things this whole time. As I got older, I kept 
+                    at it, it didn’t always involve wood, it was a multitude of things, commercials for class projects, 
+                    music videos, a guitar, drawings, etc. My love for creation has always been the thing that brings me 
+                    the most joy in life. 
+                    <br></br><br></br>
+                    Career Path
+                    <br></br>
+                    In 2015, high school was coming to an end and the deadline for applying for college was quickly approaching, 
+                    I had no idea where I was going to go or what I wanted to study. I didn’t bother applying to the University 
+                    of Washington as my GPA a was terrible and my SAT score was embarrassingly low. So, I applied to Eastern 
+                    Washington University. Go Eags! 
+                    <br></br><br></br>
+                    When I started attending EWU, I really wasn’t sure what I wanted to do. At a thanksgiving dinner during my 
+                    freshman year, I had a conversation with someone about career paths. This person told me that if I go into 
+                    software, I could work on a beach wherever I wanted, whenever I wanted. Being a malleable young man, I 
+                    thought this sounded pretty nice. I switched my degree to Computer Science the day I got back to school. 
+                    Computer Science really appealed to me because I was still building things and testing my problem-solving 
+                    skills. I don’t think there was any other degree that would have suited me better. Even though now I don’t 
+                    have a job in the software industry, I wouldn’t trade the experiences, the people I met, and the knowledge 
+                    that I gained for anything. I graduated in 2019 with a Bachelor of Science in Computer Science degree, I got
+                    a job working for the National Oceanic and Atmospheric Administration developing applications. 
+                    <br></br><br></br>
+                    I moved deeper into Seattle to be closer to work and to be out of my parents’ house. Shortly after moving, 
+                    COVID started, and I began working from home. My desk was a plastic folding table and I hated it. I started 
+                    searching for a nice desk online and soon realized I wasn’t going to find one I could afford, so I decided to
+                    build one. I already had attained the skills I needed from woodshop classes in high school, and luckily, 
+                    my brother-in-law was already into woodworking and had all the necessary tools for such a build. This was 
+                    the first piece of furniture I ever made. Little did I know what I really started with that build. 
+                    After almost 2 ½ years of working at NOAA, I was fired due to some restrictions with COVID. I spent 8 months 
+                    studying every day for interviews and applying for jobs. Only one company gave me a proper interview, Pinterest. 
+                    After a few rounds of interviews, their rejection hit me hard. I lost all motivation and desire to find a job 
+                    in the software industry. I realized that if I wanted to ever be “successful”, I couldn’t rely on other people 
+                    to decide my future for me. I knew then that I was going down a long path with lots of bumps and no guarantees. 
+                    But I was a young man with nothing to lose.
+                    <br></br><br></br>
+                    Ever since I was a kid, in the back of my mind, I always fantasized about being a YouTuber, I just never 
+                    thought it was realistic… not for me at least. Without ever having taken a risk in my life, I dropped everything 
+                    and started making videos. No, not woodworking videos, but golf videos. I started 206 Golf, and I found a new 
+                    passion in trying to get better at golf, editing, and filming. I did this for a year and a half, and really found 
+                    my editing style. At that point I had 500 subscribers, I wasn’t gaining the traction that I had hoped to by then, 
+                    but I still had no intention of giving up. I was still doing woodworking projects on the side to try to make a 
+                    little bit of cash. I decided that I wanted to film a woodworking video just for fun. I posted it, and it didn’t
+                    get very many views, which was what I expected. What I didn’t expect was my longest friend to text me telling 
+                    me how much he liked it and wanted me to hurry up and make another. Then I posted my 2nd woodworking video just 
+                    1 month later, after that, everything changed. This video got ~70,000 views and more than tripled the subscribers 
+                    I had on 206 Golf. I put 206 Golf on pause and put everything I have into Late Night Woodworking. 
+
+                    <br></br><br></br>If you have any questions or inquiries, 
+                    email me at <b>contact@latenightwoodworking.com</b>
+                    instagram and DM on there.
+                </p>
+            </div>
         </div>
     )
 }
