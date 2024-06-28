ab5e2a08c85526b0a75333541852ed538e417a05
Griffin Lockhart <lockhartgriffin@yahoo.com>
2024-06-27 20:37:12 -0700

    about

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,60 +7,68 @@ const AboutPage = () => {
         <div className='about-wrapper'>
             <div className='text'>
                 <h1>My Story.</h1>
-                <p> Where do I begin… It was a chilly Seattle evening in 1997… no, that’s too far back. But not by much.
+                <p> Where do I even start… It was a chilly Seattle evening in 1997. No, that’s too far back, But not by much.
                     <br></br><br></br>
-                    While Late Night Woodworking was established in December of 2023, the long winding story of where I 
-                    am today begins long before that.
+                    While Late Night Woodworking was established in December of 2023, the long winding story of how it came 
+                    about begins long before then. I think all the way back to elementary school, when I made my first creation, 
+                    a catapult. I built this with my father, and although I don’t remember how much I actually helped, I do 
+                    remember being really proud of it. I think that feeling even to this day has always driven me to continue 
+                    to make things. All through out grade school I have these memories of projects that I worked on. In middle 
+                    school, I made a Gatorade commercial and a slideshow music video to Superstar by Lupe Fiasco. In high 
+                    school I drew a lot more and I started taking woodshop. I made the classic cutting board in woods 1, 
+                    in woods 2 I made a sword, and in woods 3 I made an electric guitar! When I look back, it’s so obvious 
+                    how much I enjoyed doing these things, but I think in the moment, I was just going through the motions. 
+                    I was just your typical teenager playing too many sports and not doing enough homework. 
                     <br></br><br></br>
-                    Growing up, I always enjoyed making things. The first thing I remember making was a catapult, I was 
-                    in the 5th Grade. I remember being really proud of it, I think that feeling sticks with me today with 
-                    everything I make, and it’s what has kept me making things this whole time. As I got older, I kept 
-                    at it, it didn’t always involve wood, it was a multitude of things, commercials for class projects, 
-                    music videos, a guitar, drawings, etc. My love for creation has always been the thing that brings me 
-                    the most joy in life. 
+                    When it came time to apply to college, I was lost. I knew I wouldn’t be able to get into any “good” schools, 
+                    so the only school I applied to was Eastern Washington University. In the fall of 2015, I was an Eagle! I 
+                    remember when I first started college, I had the intention of getting a degree in economics. Then I met 
+                    someone at a thanksgiving dinner, and this person asked me what I was getting a degree in economics for? 
+                    I said I didn’t really know, it was one of the things in school I was okay at. He told me to look into 
+                    computer science, I could get a job making a lot of money and work on the beach whenever I wanted. I 
+                    thought that sounded pretty nice, being a young malleable man, I switched my major the day I got back 
+                    to school. It turned out to be one of the best decisions I’ve made, computer science suited me so well. 
+                    It checked all my boxes, it was challenging, I was making something, and it was full of problem solving. 
+                    I wouldn’t trade the knowledge I gained, the experiences I had, and the people I met for anything.
                     <br></br><br></br>
-                    Career Path
-                    <br></br>
-                    In 2015, high school was coming to an end and the deadline for applying for college was quickly approaching, 
-                    I had no idea where I was going to go or what I wanted to study. I didn’t bother applying to the University 
-                    of Washington as my GPA a was terrible and my SAT score was embarrassingly low. So, I applied to Eastern 
-                    Washington University. Go Eags! 
+                    In 4 years, I graduated EWU with a Bachelor of Science in Computer Science. Got a job right out of college 
+                    working for NOAA developing applications, and I really could have worked from the beach if I wanted to. 
+                    Shortly after starting my new job, COVID happened, and I was working from home. My desk was a plastic 
+                    folding table and I hated it. I started searching for a nice desk online and soon realized I wasn’t going 
+                    to find one I could afford, so I decided to build one. I already had attained the skills I needed from 
+                    woodshop classes in high school, and luckily, my brother-in-law was already into woodworking and had all 
+                    the necessary tools for such a build. This was the first piece of furniture I ever made. Little did I know 
+                    what I really started with that build. I was working there for almost 2 ½ years, then due to some restrictions 
+                    with COVID, I got fired. 
                     <br></br><br></br>
-                    When I started attending EWU, I really wasn’t sure what I wanted to do. At a thanksgiving dinner during my 
-                    freshman year, I had a conversation with someone about career paths. This person told me that if I go into 
-                    software, I could work on a beach wherever I wanted, whenever I wanted. Being a malleable young man, I 
-                    thought this sounded pretty nice. I switched my degree to Computer Science the day I got back to school. 
-                    Computer Science really appealed to me because I was still building things and testing my problem-solving 
-                    skills. I don’t think there was any other degree that would have suited me better. Even though now I don’t 
-                    have a job in the software industry, I wouldn’t trade the experiences, the people I met, and the knowledge 
-                    that I gained for anything. I graduated in 2019 with a Bachelor of Science in Computer Science degree, I got
-                    a job working for the National Oceanic and Atmospheric Administration developing applications. 
+                    Being 25 with no job and a decent amount of money saved up wasn’t too bad. I took a little bit of time to 
+                    myself, then I started grinding. I was studying every day, preparing for interviews that I was bound to get. 
+                    But time went by, and nobody would interview me. It was a frustrating thing; I was in an industry where they 
+                    claim a ton of jobs go unfulfilled every year, and here I am not able to get an interview. Then finally I got 
+                    an interview with Pinterest, I made it through all the rounds and waited for their final decision. When it 
+                    came, it hit me hard. After 8 months of hard work preparing myself to show a company that I’m worth it, I 
+                    was rejected. I knew in my bones that I was a valuable asset, how could they not see it? I decided then and 
+                    there, if I really wanted to be “successful”, I was going to have to do it on my own. I couldn’t wait for 
+                    others to catch up.
                     <br></br><br></br>
-                    I moved deeper into Seattle to be closer to work and to be out of my parents’ house. Shortly after moving, 
-                    COVID started, and I began working from home. My desk was a plastic folding table and I hated it. I started 
-                    searching for a nice desk online and soon realized I wasn’t going to find one I could afford, so I decided to
-                    build one. I already had attained the skills I needed from woodshop classes in high school, and luckily, 
-                    my brother-in-law was already into woodworking and had all the necessary tools for such a build. This was 
-                    the first piece of furniture I ever made. Little did I know what I really started with that build. 
-                    After almost 2 ½ years of working at NOAA, I was fired due to some restrictions with COVID. I spent 8 months 
-                    studying every day for interviews and applying for jobs. Only one company gave me a proper interview, Pinterest. 
-                    After a few rounds of interviews, their rejection hit me hard. I lost all motivation and desire to find a job 
-                    in the software industry. I realized that if I wanted to ever be “successful”, I couldn’t rely on other people 
-                    to decide my future for me. I knew then that I was going down a long path with lots of bumps and no guarantees. 
-                    But I was a young man with nothing to lose.
+                    At this point I had already started making YouTube videos, but the videos weren’t about woodworking. My first 
+                    two videos were wakeboarding videos. Making those videos awakened something in me, I had this digital canvas, 
+                    and I could paint anything. So, when I realized that I was going down my own path, I knew YouTube was the way.
+                    That’s when 206 Golf was born, I was so dedicated to it that I burned through my savings, moved back into my
+                     parents, somehow found myself with more debt than I accrued in college (thanks to Washington’s ESD), but 
+                     I was committed. What can I say? I was/am a young man with nothing to lose. Nothing was going to stop me 
+                     from achieving what I thought was my dream, until everything changed. Throughout the year and a half of 
+                     making golf videos, I was woodworking, trying to make some money on the side. I got into watching other 
+                     YouTubers make woodworking videos and I wanted to give it a shot, just for fun. I posted my first video, 
+                     making a chessboard. I received a text from one of my best friends who hated my golf videos, he said “I 
+                     want more.” So, I quickly made my second video, the Elmwood Foyer Table. On Christmas day, that video got 
+                     around 20k views and was still going up. I really couldn’t have asked for a better gift. In all the time 
+                     making the golf videos, I only got up to around 500 subs. From this one woodworking video, I got 1,500 subs. 
+                     I dropped everything. Since Christmas day in 2023, I’ve been putting all my time and energy into Late Night 
+                     Woodworking and 7 months in (today), I’m at just over 5,000 subs. 
                     <br></br><br></br>
-                    Ever since I was a kid, in the back of my mind, I always fantasized about being a YouTuber, I just never 
-                    thought it was realistic… not for me at least. Without ever having taken a risk in my life, I dropped everything 
-                    and started making videos. No, not woodworking videos, but golf videos. I started 206 Golf, and I found a new 
-                    passion in trying to get better at golf, editing, and filming. I did this for a year and a half, and really found 
-                    my editing style. At that point I had 500 subscribers, I wasn’t gaining the traction that I had hoped to by then, 
-                    but I still had no intention of giving up. I was still doing woodworking projects on the side to try to make a 
-                    little bit of cash. I decided that I wanted to film a woodworking video just for fun. I posted it, and it didn’t
-                    get very many views, which was what I expected. What I didn’t expect was my longest friend to text me telling 
-                    me how much he liked it and wanted me to hurry up and make another. Then I posted my 2nd woodworking video just 
-                    1 month later, after that, everything changed. This video got ~70,000 views and more than tripled the subscribers 
-                    I had on 206 Golf. I put 206 Golf on pause and put everything I have into Late Night Woodworking. 
-
+                    So, my dear reader, you may be wondering why I have told you all of this on the about page of a woodworking website. 
+                    Well, this… is my story. Not finished joe.
                     <br></br><br></br>If you have any questions or inquiries, 
                     email me at <b>contact@latenightwoodworking.com</b>
                     instagram and DM on there.
